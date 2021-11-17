cfe74fca090e54d51d0d0a61d8a6848579cfb3ca
dennisluma <ilumadennis20@gmail.com>
2021-11-17 08:49:27 +0100

    initialized project

---
+++ src/main/java/come/dennis/s3imageupload/S3ImageUploadApplication.java
@@ -0,0 +1,13 @@
+package come.dennis.s3imageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ImageUploadApplication.class, args);
+	}
+
+}
