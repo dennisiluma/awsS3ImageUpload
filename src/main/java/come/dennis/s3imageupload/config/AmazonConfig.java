5fe0fa0c35cbdb98b8b8ab5a1edcac1a121182bd
dennisluma <ilumadennis20@gmail.com>
2021-11-18 11:06:27 +0100

    modified keys

--- src/main/java/come/dennis/s3imageupload/config/AmazonConfig.java
+++ src/main/java/come/dennis/s3imageupload/config/AmazonConfig.java
@@ -15,34 +15,6 @@ import java.util.Scanner;
 
 @Configuration
 public class AmazonConfig {
-
-    String key;
-    String id;
-
-        public void awsKey(){
-            try {
-                File myObj = new File("awsaccesskey.txt");
-                Scanner myReader = new Scanner(myObj);
-                while (myReader.hasNextLine()) {
-                    String data = myReader.nextLine();
-                    id = data;
-                }
-                myReader.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-            try {
-                File myObj2 = new File("awssecretkey.txt");
-                Scanner myReader = new Scanner(myObj2);
-                while (myReader.hasNextLine()) {
-                    String data2 = myReader.nextLine();
-                    key = data2;
-                }
-                myReader.close();
-            } catch (FileNotFoundException e) {}
-        }
-
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
